c9b2ff6dba10908d3afc4249f2f72858a57c0e3b
Neil Campbell <neildcampbell@hotmail.com>
2019-10-02 17:24:38 +0100

    add NoteController

--- note-controller.js
+++ note-controller.js
@@ -1,3 +1,4 @@
+// var nl = new NoteList()
 var element = document.getElementById("app");
 console.log(element);
 
@@ -6,3 +7,31 @@ function changeGreeting(){
 }
 
 changeGreeting();
+
+(function(exports){
+  function NoteController(NoteList){
+    this.NoteList = NoteList;
+  }
+
+  NoteController.prototype.addNote = function(text){
+    var note = this.NoteList.createNote(text)
+    return note
+  }
+
+  NoteController.prototype.view = function(){
+    var note = new NoteListView(this.NoteList)
+    note.returnsView()
+    return element.innerHTML = note.returnsView()
+  }
+  exports.NoteController = NoteController
+})(this)
+
+
+// console.log("nl = " + nl)
+// var newcontrol = new NoteController(nl)
+// newcontrol.addNote("Favourite drink: seltzer")
+// newcontrol.view()
+// console.log(newcontrol.view())
+
+
+
