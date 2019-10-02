b5e92c2420ead2e6c7661bd9495981007f2d99c1
Neil Campbell <neildcampbell@hotmail.com>
2019-10-02 15:14:17 +0100

    added notelist view

--- note-list-view-test.js
+++ note-list-view-test.js
@@ -2,8 +2,12 @@ function returnsListOfNotes(){
   nameOfFunc = arguments.callee.toString().match(/function\s+([^\s\(]+)/);
     noteList = new NoteList();
     noteList.createNote("This is my new note")
+    
     noteList.createNote("This is the second note")
+    var array = noteList.listOfNotes()
+    console.log("array = " + array[0].text)
     noteListView = new NoteListView(noteList)
+    console.log("noteListView = " + noteListView)
     assert.isTrue(noteListView.returnsView() === `<ul><li><div>This is my new note</div></li><li><div>This is the second note</div></li></ul>`)
   }
 
--- note-list-view.js
+++ note-list-view.js
@@ -1,4 +1,20 @@
-(function NoteListView(exports){
-  
+(function (exports){
+  function NoteListView(){
+
+  }
   exports.NoteListView = NoteListView
 })(this)
+
+NoteListView.prototype.returnsView = function(){
+  // console.log("new notelist"  + new NoteList())
+  var noteList = new NoteList()
+  var array = noteList.listOfNotes();
+
+  console.log(array)
+  var before = "<ul>"
+  for (var i = 0;i< array.length;i++){
+    return  before += "<li><div>" +  array[i].text + "</div></li></ul>"
+  } 
+  
+  
+}
