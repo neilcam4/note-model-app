bd329a0d2e60be93306d4d29365165c2200ad2df
Neil Campbell <neildcampbell@hotmail.com>
2019-10-03 16:58:58 +0100

    added test to limit chatacters

--- note-controller-test.js
+++ note-controller-test.js
@@ -5,9 +5,7 @@ function insertHTMLIntoDom(){
     var notecon = new NoteController(new NoteList())
     
     notecon.view()
-    console.log(document.getElementById("app").innerHTML)
-    console.log("<ul><li><div>Favorite drink: Seltzer</div></li></ul>")
-    assert.isTrue(document.getElementById("app").innerHTML === "<ul><li><div>Favorite drink: Seltzer</div></li></ul>", nameOfFunc)    
+    assert.isTrue(document.getElementById("app").innerHTML === "<ul><li><div>Favorite drink: Selt</div></li></ul>", nameOfFunc)    
 }
 
 insertHTMLIntoDom()
--- note-list-view.js
+++ note-list-view.js
@@ -7,7 +7,7 @@
     var array = this.noteList.listOfNotes();
     var before = "<ul>"
     array.forEach(function(item){
-      before += "<li><div>" +  item.returnText() + "</div></li>"
+      before += "<li><div>" +  item.returnText().slice(0,20) + "</div></li>"
     })
     return before += "</ul>" 
   }
